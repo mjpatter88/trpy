0c8a7e82a6b67174b2ce1e4207433e3341d2f7ce
Michael Patterson <mjpatter88@gmail.com>
2016-12-10 15:45:13 -0600

    Fourth attempt at line drawing.

--- render.py
+++ render.py
@@ -9,16 +9,15 @@ def draw_point(image, point, color):
     image.putpixel(new_point, color)
 
 def draw_line(image, start, end, color):
-    # Always draw from left to right
-    if start[0] > end[0]:
-        start, end = end, start
-
     steep = False;
     if abs(start[0] - end[0]) < abs(start[1] - end[1]):
         steep = True
         start = transpose(start)
         end = transpose(end)
 
+    # Always draw from left to right
+    if start[0] > end[0]:
+        start, end = end, start
 
     x0, y0 = start
     x1, y1 = end
@@ -35,6 +34,7 @@ image = Image.new('RGB', (100, 100))
 draw_line(image, (13,20), (80,40), (255, 255, 255))
 draw_line(image, (20,13), (40,80), (255, 0, 0))
 draw_line(image, (80,40), (13,20), (255, 0, 0))
+draw_line(image, (20,80), (40,20), (255, 0, 0))
 
 image = image.transpose(Image.FLIP_TOP_BOTTOM)
 image.save('test.png')
