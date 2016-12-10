34252f8cd585a0754cb7fd98f9eefacd3d59b9b2
Michael Patterson <mjpatter88@gmail.com>
2016-12-10 15:04:37 -0600

    Third attempt at line drawing.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A Tiny Renderer in Python
 ![one-red-pixel](images/home.png)
 ![first-line](images/1_0.png)
 ![second-lines](images/1_1.png)
+![third-lines](images/1_2.png)
--- render.py
+++ render.py
@@ -1,21 +1,37 @@
 from PIL import Image
 
 
+def transpose(point):
+    return point[1], point[0]
+
 def draw_point(image, point, color):
     new_point = int(point[0]), int(point[1])
     image.putpixel(new_point, color)
 
 def draw_line(image, start, end, color):
-    step = 10
-    for x in range(start[0], end[0]):
-        step = (x-start[0])/(end[0]-start[0])
-        y = start[1]*(1-step) + end[1]*step
+    # Always draw from left to right
+    if start[0] > end[0]:
+        start, end = end, start
+
+    steep = False;
+    if abs(start[0] - end[0]) < abs(start[1] - end[1]):
+        steep = True
+        start = transpose(start)
+        end = transpose(end)
+
+
+    x0, y0 = start
+    x1, y1 = end
+    for x in range(x0, x1):
+        step = (x-x0)/(x1-x0)
+        y = y0*(1-step) + y1*step
+        if steep:
+            x, y = transpose((x,y))
         draw_point(image, (x,y), color)
 
 
 image = Image.new('RGB', (100, 100))
 
-draw_point(image, (52, 41), (255,0,0))
 draw_line(image, (13,20), (80,40), (255, 255, 255))
 draw_line(image, (20,13), (40,80), (255, 0, 0))
 draw_line(image, (80,40), (13,20), (255, 0, 0))
