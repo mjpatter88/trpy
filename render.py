9f26b7a704d83ddcc3e59cbe880fee8090789d3c
Michael Patterson <mpatterson@pillartechnology.com>
2016-12-16 14:23:44 -0600

    Refactor to write to an array of pixels and then write whole array to image file. Add tests around point and line drawing.

--- render.py
+++ render.py
@@ -7,9 +7,9 @@ from model import Model
 def transpose(point):
     return point[1], point[0]
 
-def draw_point(image, point, color):
-    new_point = int(point[0]), int(point[1])
-    image.putpixel(new_point, color)
+def draw_point(pixels, point, color):
+    x, y = int(point[0]), int(point[1])
+    pixels[x][y] = color
 
 def line_to_pixels(start, end):
     pixels = []
@@ -35,43 +35,61 @@ def line_to_pixels(start, end):
         pixels.append((x,y))
     return pixels
 
-def draw_line(image, start, end, color):
+def draw_line(pixels, start, end, color):
     for pixel in line_to_pixels(start, end):
-        draw_point(image, pixel, color)
+        draw_point(pixels, pixel, color)
 
-def draw_hor_line(image, start, end, color):
+def draw_hor_line(pixels, start, end, color):
     x1 = min(start[0], end[0])
     x2 = max(start[0], end[0])
-    for x in range(x1, x2):
-        draw_point(image, (x, start[1]), color)
+    for x in range(x1, x2 + 1):
+        draw_point(pixels, (x, start[1]), color)
 
-def draw_between_sides(image, side_1, side_2, color):
+def draw_between_sides(pixels, side_1, side_2, color):
     side_1 = {point[1]:point[0] for point in side_1}
     side_2 = {point[1]:point[0] for point in side_2}
     for y in side_1:
         if y in side_2:
-            draw_hor_line(image, (side_1[y], y), (side_2[y], y), color)
+            draw_hor_line(pixels, (side_1[y], y), (side_2[y], y), color)
 
-def draw_triangle(image, points, color):
-    draw_line(image, points[0], points[1], color)
-    draw_line(image, points[1], points[2], color)
-    draw_line(image, points[0], points[2], color)
+def draw_triangle(pixels, points, color):
+    draw_line(pixels, points[0], points[1], color)
+    draw_line(pixels, points[1], points[2], color)
+    draw_line(pixels, points[0], points[2], color)
 
-def fill_triangle(image, points, color):
+def fill_triangle(pixels, points, color):
     side_1 = line_to_pixels(points[0], points[1])
     side_2 = line_to_pixels(points[1], points[2])
     side_3 = line_to_pixels(points[0], points[2])
 
-    draw_between_sides(image, side_1, side_2, color)
-    draw_between_sides(image, side_2, side_3, color)
-    draw_between_sides(image, side_1, side_3, color)
+    draw_between_sides(pixels, side_1, side_2, color)
+    draw_between_sides(pixels, side_2, side_3, color)
+    draw_between_sides(pixels, side_1, side_3, color)
 
 
-def draw_model_wireframe():
+def render_model_wireframe(model_file, width, height, pixels):
+    model = Model(model_file, width, height)
+
+    for face in model.get_faces():
+        color = (int(random.random()*255), int(random.random()*255), int(random.random()*255))
+        fill_triangle(pixels, face, color)
+
+def write_pixels_to_image_file(width, height, pixels, out_file_name):
+    import itertools
+    im = Image.new('RGB', (width, height), "black")
+    im_pixels = im.load()
+
+    for x,y in itertools.product(range(im.height), range(im.width)):
+        im_pixels[x, y] = pixels[x][y]
+
+    im = im.transpose(Image.FLIP_TOP_BOTTOM)
+    im.save(out_file_name)
+
+
+if __name__ == '__main__':
     model_file = 'models/african_head.obj'
     width = 500
     height = 500
-    color = (255, 255, 255)
     out_file_name = 'out_render.png'
 
     if len(sys.argv) > 1:
@@ -80,27 +98,8 @@ def draw_model_wireframe():
         width = int(sys.argv[2])
         height = int(sys.argv[3])
 
-    image = Image.new('RGB', (width, height))
-    model = Model(model_file, width, height)
-
-    for face in model.get_faces():
-        color = (int(random.random()*255), int(random.random()*255), int(random.random()*255))
-        fill_triangle(image, face, color)
-
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    image.save(out_file_name)
-
-def foo():
-    import itertools
-    im = Image.new('RGB', (255,255), "black")
-    pixels = im.load()
-
-    for x,y in itertools.product(range(im.height), range(im.width)):
-        pixels[x, y] = (255,255,255)
-    im.save('test_out.png')
-
-
-if __name__ == '__main__':
-    draw_model_wireframe()
+    pixels = [[(0,0,0) for x in range(width)] for y in range(height)]
+    render_model_wireframe(model_file, width, height, pixels)
+    write_pixels_to_image_file(width, height, pixels, out_file_name)
 
 
--- render_test.py
+++ render_test.py
@@ -1,4 +1,4 @@
-from render import transpose
+from render import transpose, draw_point, draw_hor_line
 
 
 def test_transpose():
@@ -6,3 +6,33 @@ def test_transpose():
     point = transpose(point)
 
     assert point == (1, 0)
+
+def test_draw_point():
+    p = [[(0,0,0) for x in range(5)] for y in range(5)]
+    color = (255,255,255)
+    draw_point(p, (2,3), color)
+
+    assert p[2][3] == color
+
+def test_draw_hor_line():
+    p = [[(0,0,0) for x in range(10)] for y in range(10)]
+    color = (255,255,255)
+
+    draw_hor_line(p, (2,3), (5,3), color)
+
+    assert p[2][3] == color
+    assert p[3][3] == color
+    assert p[4][3] == color
+    assert p[5][3] == color
+
+def test_draw_hor_line_backwards():
+    p = [[(0,0,0) for x in range(10)] for y in range(10)]
+    color = (255,255,255)
+
+    draw_hor_line(p, (5,4), (2,4), color)
+
+    assert p[2][4] == color
+    assert p[3][4] == color
+    assert p[4][4] == color
+    assert p[5][4] == color
+
