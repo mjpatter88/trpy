774cbcf56bfe2727483dc165ab48a81c04555d03
Michael Patterson <mjpatter88@gmail.com>
2017-10-29 01:27:57 -0500

    Add all pixels at once rather than one by one.

--- render.py
+++ render.py
@@ -7,17 +7,19 @@ from model import Model
 WIDTH = 800
 HEIGHT = 800
 
+pixels = [0] * WIDTH * HEIGHT
+
 def transpose(point):
     return point[1], point[0]
 
-def draw_point(image, point, color):
+def draw_point(point, color):
     x = min(int(point[0]), WIDTH-1)
     y = min(int(point[1]), HEIGHT-1)
-    new_point = x, y
-    image.putpixel(new_point, color)
+    new_point = x + (y * WIDTH)
+    pixels[new_point] = color
 
 def line_to_pixels(start, end):
-    pixels = []
+    pix = []
     start = round((start[0])), round((start[1]))
     end = round((end[0])), round((end[1]))
     steep = False;
@@ -34,46 +36,46 @@ def line_to_pixels(start, end):
     x1, y1 = end
     # If line starts and stops on the same point, just return that point
     if x0 == x1:
-        pixels.append((x0,y0))
+        pix.append((x0,y0))
     else:
         for x in range(x0, x1+1):
             step = (x-x0)/(x1-x0)
             y = round((y0*(1-step) + y1*step))
             if steep:
                 x, y = transpose((x,y))
-            pixels.append((x,y))
-    return pixels
+            pix.append((x,y))
+    return pix
 
-def draw_line(image, start, end, color):
+def draw_line(start, end, color):
     for pixel in line_to_pixels(start, end):
-        draw_point(image, pixel, color)
+        draw_point(pixel, color)
 
-def draw_hor_line(image, start, end, color):
+def draw_hor_line(start, end, color):
     x1 = min(start[0], end[0])
     x2 = max(start[0], end[0])
     for x in range(x1, x2):
-        draw_point(image, (x, start[1]), color)
+        draw_point((x, start[1]), color)
 
-def draw_between_sides(image, side_1, side_2, color):
+def draw_between_sides(side_1, side_2, color):
     side_1 = {point[1]:point[0] for point in side_1}
     side_2 = {point[1]:point[0] for point in side_2}
     for y in side_1:
         if y in side_2:
-            draw_hor_line(image, (side_1[y], y), (side_2[y], y), color)
+            draw_hor_line((side_1[y], y), (side_2[y], y), color)
 
-def draw_triangle(image, points, color):
-    draw_line(image, points[0], points[1], color)
-    draw_line(image, points[1], points[2], color)
-    draw_line(image, points[0], points[2], color)
+def draw_triangle(points, color):
+    draw_line(points[0], points[1], color)
+    draw_line(points[1], points[2], color)
+    draw_line(points[0], points[2], color)
 
-def fill_triangle(image, points, color):
+def fill_triangle(points, color):
     side_1 = line_to_pixels(points[0], points[1])
     side_2 = line_to_pixels(points[1], points[2])
     side_3 = line_to_pixels(points[0], points[2])
 
-    draw_between_sides(image, side_1, side_2, color)
-    draw_between_sides(image, side_2, side_3, color)
-    draw_between_sides(image, side_1, side_3, color)
+    draw_between_sides(side_1, side_2, color)
+    draw_between_sides(side_2, side_3, color)
+    draw_between_sides(side_1, side_3, color)
 
 """
 Do a basic matrix cross product
@@ -131,9 +133,10 @@ def draw_model_wireframe():
         # If intensity < 0, polygon is not visible (roughly speaking)
         if intensity > 0:
             color = (int(intensity*255), int(intensity*255), int(intensity*255))
-            fill_triangle(image, screen_face, color)
+            fill_triangle(screen_face, color)
             # draw_triangle(image, screen_face, (color[0], 0, 0))
 
+    image.putdata(pixels)
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
     image.save(out_file_name)
 
